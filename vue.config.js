3cfe11b7441a234ee978707fc9a32737acd04115
yinziming <i@yinziming.com>
2023-07-31 16:07:17 +0000

    v1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/chatgpt/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/h3-tool/' : '/',
   transpileDependencies: true
 })
